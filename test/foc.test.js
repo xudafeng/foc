36c3479593c01462ae1bf1249af1e71a1b91b9cf
dafeng.xdf <dafeng.xdf@taobao.com>
2014-10-26 23:45:43 +0800

    First Commit

---
+++ test/foc.test.js
@@ -0,0 +1,22 @@
+/* ================================================================
+ * foc by xdf(xudafeng[at]126.com)
+ *
+ * first created at : Thu Oct 23 2014 01:26:19 GMT+0800 (CST)
+ *
+ * ================================================================
+ * Copyright 2013 xdf
+ *
+ * Licensed under the MIT License
+ * You may not use this file except in compliance with the License.
+ *
+ * ================================================================ */
+
+'use strict';
+
+var foc = require('../');
+
+describe('/lib/foc.js', function(){
+  describe('main', function() {
+    console.log(foc)
+  });
+});
